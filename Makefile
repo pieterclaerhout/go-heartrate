7acb8c8b9b18a9703a0a9200206ca93be1747e27
Pieter Claerhout <pieter@yellowduck.be>
2019-10-03 10:04:03 +0200

    Added the pacer example

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
-run:
-	go build -o hartrate/hartrate github.com/pieterclaerhout/go-sports/hartrate
-	./hartrate/hartrate
+run-hr:
+	go build -o heartrate/heartrate github.com/pieterclaerhout/go-sports/heartrate
+	./heartrate/heartrate
+
+run-pacer:
+	go build -o pacer/pacer github.com/pieterclaerhout/go-sports/pacer
+	@echo
+	./pacer/pacer -distance 10 -duration 60m -endo
+	@echo
+	./pacer/pacer -distance 31.7 -duration 61m -endo
---
+++ pacer/pacer.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+)
+
+var (
+	distance = flag.Float64("distance", 0.0, "total distance of the training (km)")
+	duration = flag.Duration("duration", 0, "total duration of the training")
+	endo     = flag.Bool("endo", false, "print a comment for Endomondo")
+)
+
+func main() {
+
+	flag.Parse()
+
+	seconds := duration.Seconds()
+	secPerKm := seconds / *distance
+
+	pace := formatPace(secPerKm)
+	fmt.Printf("Pace (min/km): %v\n", pace)
+
+	speed := math.Floor(100*3600.0/secPerKm) / 100.0
+	fmt.Printf("Speed (km/h): %v\n", speed)
+
+	if *endo {
+		fmt.Printf("Distance: %vkm, ~%vmin/km, ~%vkm/h\n", *distance, pace, speed)
+	}
+
+}
+
+func formatPace(secsPerKm float64) string {
+	secs := int(secsPerKm)
+	mins, secs := int(math.Floor(float64(secs/60))), secs%60
+	return fmt.Sprintf("%02d:%02d", int(mins), int(secs))
+}
